56d9354b0145b8178e5a9567d0a0ddb6fed5da0c
Robert Grembowicz <robert.grembowicz@gmail.com>
2021-07-13 18:21:25 +0200

    change protocol from http to https in links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,3 +1,3 @@
-export const apiWeatherUrl = 'http://api.openweathermap.org/data/2.5';
+export const apiWeatherUrl = 'https://api.openweathermap.org/data/2.5';
 export const weatherIcon = (icon: string) =>
-  `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  `https://openweathermap.org/img/wn/${icon}@2x.png`;
